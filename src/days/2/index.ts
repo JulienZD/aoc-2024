a9324f4fec6bdeca3d4c0f1d70bdaf325bfa5abd
Julien Zapata Duque <jzapata@live.nl>
2024-12-02 09:57:51 +0100

    feat: day 2 part 2

--- src/days/2/index.ts
+++ src/days/2/index.ts
@@ -3,40 +3,46 @@ import type { Solver } from '../../solution.js';
 export const part1: Solver = (input) => {
   const report = input.map((line) => line.split(' ').map(Number));
 
-  const validIndexes: number[] = [];
-
-  for (let lineIndex = 0; lineIndex < report.length; lineIndex++) {
-    const line = report[lineIndex]!;
-
-    let isValid = true;
-
-    let prevIncrements: boolean | undefined;
-
-    for (let i = 0; i < line.length - 1; i++) {
-      const current = line[i]!;
-      const next = line[i + 1]!;
-
-      const diff = current - next;
-      if (Math.abs(diff) > 3 || diff === 0) {
-        isValid = false;
-        break;
-      }
-
-      const increments = diff > 0;
-      if (prevIncrements !== undefined && prevIncrements !== increments) {
-        isValid = false;
-        break;
-      }
-
-      prevIncrements = increments;
-    }
-
-    if (isValid) {
-      validIndexes.push(lineIndex);
-    }
-  }
-
-  return validIndexes.length;
+  return report.filter((line) => isValidLine(line)).length;
 };
 
-export const part2: Solver = (input) => {};
+export const part2: Solver = (input) => {
+  const report = input.map((line) => line.split(' ').map(Number));
+
+  return report.map((line) => isValidLinePart2(line)).filter(Boolean).length;
+};
+
+function isValidLine(line: number[]) {
+  let isValid = true;
+
+  let prevIncrements: boolean | undefined;
+
+  for (let i = 0; i < line.length - 1; i++) {
+    const current = line[i]!;
+    const next = line[i + 1]!;
+
+    const diff = current - next;
+    if (Math.abs(diff) > 3 || diff === 0) {
+      isValid = false;
+      break;
+    }
+
+    const increments = diff > 0;
+    if (prevIncrements !== undefined && prevIncrements !== increments) {
+      isValid = false;
+      break;
+    }
+
+    prevIncrements = increments;
+  }
+
+  return isValid;
+}
+
+function isValidLinePart2(line: number[]) {
+  if (isValidLine(line)) {
+    return true;
+  }
+
+  return line.some((_, indexToRemove) => isValidLine(line.filter((_, index) => index !== indexToRemove)));
+}
