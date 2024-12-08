26f66c7e9ce37dcd0f3e35f732d93edafda6489d
Julien Zapata Duque <jzapata@live.nl>
2024-12-08 09:27:56 +0100

    feat: day 7 part 2

--- src/days/7/index.ts
+++ src/days/7/index.ts
@@ -15,7 +15,19 @@ export const part1: Solver = (input) => {
   return calibrations.reduce<number>((total, current) => total + current.target, 0);
 };
 
-export const part2: Solver = (input) => {};
+export const part2: Solver = (input) => {
+  const calibrations = input
+    .map((line) => {
+      const [_, target, equations] = line.match(/(\d+): (.*)/);
+      return {
+        target: Number(target),
+        equations: equations.split(' ').map(Number),
+      };
+    })
+    .filter(({ target, equations }) => canReachTarget2(target, ...equations));
+
+  return calibrations.reduce<number>((total, current) => total + current.target, 0);
+};
 
 function canReachTarget(target: number, ...numbers: number[]) {
   return recurse(target, numbers[0]!, numbers[1]!, ...numbers.slice(2));
@@ -34,9 +46,32 @@ function recurse(target: number, left: number, right: number, ...next: number[])
   }
 
   const [nextNum, ...nextNumbers] = next;
+  // This is somehow not breaking for part 1, but is for part 2
   if (nextNum === undefined) {
     return false;
   }
 
   return recurse(target, multiplied, nextNum, ...nextNumbers) || recurse(target, added, nextNum, ...nextNumbers);
+}
+
+function canReachTarget2(target: number, ...numbers: number[]) {
+  return recurse2(target, numbers[0]!, numbers[1]!, ...numbers.slice(2));
+}
+
+function recurse2(target: number, left: number, right: number | undefined, ...next: number[]): boolean {
+  if (right === undefined) {
+    return left === target;
+  }
+
+  const multiplied = left * right;
+  const added = left + right;
+  const concatenated = Number(`${left}${right}`);
+
+  const [nextNum, ...nextNumbers] = next;
+
+  return (
+    recurse2(target, added, nextNum, ...nextNumbers) ||
+    recurse2(target, multiplied, nextNum, ...nextNumbers) ||
+    recurse2(target, concatenated, nextNum, ...nextNumbers)
+  );
 }
